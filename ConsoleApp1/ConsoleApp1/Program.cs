5a85e4b6aabbc28997154c6c2900748bada72e24
Yuriy Malyshenko <yura19988991@gmail.com>
2024-10-21 16:26:57 +0500

    добавил консольное приложение

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿public interface IDiscount
+{
+    double GetDiscount();
+}
+
+public class RegularCustomerDiscount : IDiscount
+{
+    public double GetDiscount() => 0.1;
+}
+
+public class PremiumCustomerDiscount : IDiscount
+{
+    public double GetDiscount() => 0.2;
+}
+
+public class DiscountCalculator
+{
+    public double CalculateDiscount(IDiscount discount)
+    {
+        return discount.GetDiscount();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DiscountCalculator calculator = new DiscountCalculator();
+
+// Для обычного клиента
+        IDiscount regularDiscount = new RegularCustomerDiscount();
+        double regularCustomerDiscount = calculator.CalculateDiscount(regularDiscount); // Вернет 0.1
+        Console.WriteLine(regularCustomerDiscount);
+
+// Для премиум клиента
+        IDiscount premiumDiscount = new PremiumCustomerDiscount();
+        double premiumCustomerDiscount = calculator.CalculateDiscount(premiumDiscount); // Вернет 0.2
+    } 
+}
